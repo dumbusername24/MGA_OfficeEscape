73d4cba1d0639f887545344d51367ee98284fb36
Konstantin Trinkl <konstantrinkl@gmail.com>
2021-01-13 17:14:31 +0100

    added UI to main

---
+++ Assets/Scripts/MenuHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+
+public class MenuHandler : MonoBehaviour
+{
+    public GameObject MainMenu;
+    public GameObject PauseMenu;
+
+    public void PlayGame()
+    {
+        //this has to be the index in the build queue (file > build settings)
+        SceneManager.LoadScene(2);
+    }
+
+    //public void BackToMainMenu()
+    //{
+    //    //this has to be the index in the build queue (file > build settings)
+    //    PauseMenu.SetActive(false);
+    //    MainMenu.SetActive(true);
+    //}
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
