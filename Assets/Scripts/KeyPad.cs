ce6408bfd3554eb7e31cc650288506877dac1ed7
if18b120 <48284379+if18b120@users.noreply.github.com>
2021-01-14 03:59:07 +0100

    enter digits done, code check and integration missing

---
+++ Assets/Scripts/KeyPad.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class KeyPad : MonoBehaviour
+{
+    public GameObject[] Digits;
+    private int index = 0;
+    public void DigitInput(int num)
+    {
+        TextMeshProUGUI text = Digits[index].GetComponent<TextMeshProUGUI>();
+        text.SetText("" + num);
+        if (index >= 3)
+        {
+            index= 0;
+        }
+        else
+        {
+            index++;
+        }
+    }
+    
+}
