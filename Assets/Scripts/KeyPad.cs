889ce0defd8a05d66845777f0a4dee010fd18717
if18b120 <48284379+if18b120@users.noreply.github.com>
2021-01-14 17:45:19 +0100

    keycode mostly working

--- Assets/Scripts/KeyPad.cs
+++ Assets/Scripts/KeyPad.cs
@@ -5,14 +5,42 @@ using TMPro;
 
 public class KeyPad : MonoBehaviour
 {
-    public GameObject[] Digits;
+    public GameObject[] digits;
+    public GameObject red;
+    public GameObject green;
+    private int[] inputNumbers;
     private int index = 0;
+
+    public void Start()
+    {
+        inputNumbers = new int[4];
+    }
     public void DigitInput(int num)
     {
-        TextMeshProUGUI text = Digits[index].GetComponent<TextMeshProUGUI>();
+        TextMeshProUGUI text; 
+        if (index == 0)
+        {
+            foreach (GameObject item in digits)
+            {
+                text = item.GetComponent<TextMeshProUGUI>();
+                text.SetText("" + 0);
+                red.SetActive(false);
+                green.SetActive(false);
+            }
+        }
+        text = digits[index].GetComponent<TextMeshProUGUI>();
         text.SetText("" + num);
+        inputNumbers[index] = num;
         if (index >= 3)
         {
+            if (inputNumbers[0] == 1 && inputNumbers[1] == 1 && inputNumbers[2] == 1 && inputNumbers[3] == 1)
+            {
+                green.SetActive(true);
+            }
+            else
+            {
+                red.SetActive(true);
+            }
             index= 0;
         }
         else
