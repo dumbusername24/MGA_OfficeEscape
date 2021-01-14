8ecdcde076fa9d8433baf0ffebf98614d73db9d9
Konstantin Trinkl <konstantrinkl@gmail.com>
2021-01-14 12:01:00 +0100

    inventory sprites are visible

--- Assets/Scripts/InventoryLogic.cs
+++ Assets/Scripts/InventoryLogic.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -20,14 +21,29 @@ public class InventoryLogic : MonoBehaviour
     public void SetInventory(Inventory inventory)
     {
         this.inventory = inventory;
+
+        inventory.OnItemListChanged += Inventory_OnItemListChanged;
+        RefreshInventoryItems();
+    }
+
+    private void Inventory_OnItemListChanged(object sender, EventArgs e)
+    {
         RefreshInventoryItems();
     }
     
     private void RefreshInventoryItems()
     {
+        foreach(Transform child in itemSlotContainer)
+        {
+            if(child == itemSlotTemplate)
+            {
+                continue;
+            }
+            Destroy(child.gameObject);
+        }
         int x = 0;
         int y = 0;
-        float itemSlotCellSize = 64f;
+        float itemSlotCellSize = 90f;
         foreach (Item item in inventory.GetItemList())
         {
             RectTransform itemSlotRectTransform = Instantiate(itemSlotTemplate, itemSlotContainer).GetComponent<RectTransform>();
