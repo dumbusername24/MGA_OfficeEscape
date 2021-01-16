609e0f28451af4d6e6d8114b00b32fa0f04cf7f6
if18b120 <48284379+if18b120@users.noreply.github.com>
2021-01-16 19:02:10 +0100

    highscore finished

---
+++ Assets/Scripts/ShowFinalTime.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ShowFinalTime : MonoBehaviour
+{
+    private void Awake()
+    {
+        if (Timer.seconds < 10)
+        {
+            this.GetComponent<TextMeshProUGUI>().SetText(Timer.minutes + ":0" + Timer.seconds);
+        }
+        else
+        {
+            this.GetComponent<TextMeshProUGUI>().SetText(Timer.minutes + ":" + Timer.seconds);
+        }
+        Timer.SaveGame();
+        Timer.Reset();
+    }
+    
+}
---
+++ Assets/Scripts/ShowHighScore.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ShowHighScore : MonoBehaviour
+{
+    private void Awake()
+    {
+        if (PlayerPrefs.HasKey("HighScoreSeconds"))
+        {
+            if (PlayerPrefs.GetInt("HighScoreSeconds") < 10)
+            {
+                this.GetComponent<TextMeshProUGUI>().SetText(PlayerPrefs.GetInt("HighScoreMinutes") + ":0" + PlayerPrefs.GetInt("HighScoreSeconds"));
+            }
+            else
+            {
+                this.GetComponent<TextMeshProUGUI>().SetText(PlayerPrefs.GetInt("HighScoreMinutes") + ":" + PlayerPrefs.GetInt("HighScoreSeconds"));
+            }
+        }
+        else
+        {
+            this.GetComponent<TextMeshProUGUI>().SetText("No Highscore");
+        }
+    }
+
+    public void ClearSave()
+    {
+        PlayerPrefs.DeleteAll();
+        Awake();
+    }
+}
